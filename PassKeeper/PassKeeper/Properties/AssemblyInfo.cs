cac5c3b97b4441edbc8484eb4bb10e4c5f4fe62e
Wolf9152 <thomaswolf91@gmail.com>
2015-11-22 21:21:47 +0100

    PassKeeperInit
    A projekt létrehozása

---
+++ PassKeeper/PassKeeper/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PassKeeper")]
+[assembly: AssemblyDescription("It can encrypt you passwords and save them to a file...")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("...")]
+[assembly: AssemblyProduct("PassKeeper")]
+[assembly: AssemblyCopyright("Farkas Tamás Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("66ecb0cb-30ad-4708-ab1e-bef03facde17")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("hu-HU")]
+
